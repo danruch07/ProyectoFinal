039460a906661e0764df711ae77577c8a015efd1
danruch07 <danruch07@gmail.com>
2023-12-06 02:48:47 -0600

    primer commit

---
+++ android/app/src/main/kotlin/com/example/alianza_troyana/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.alianza_troyana
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/SplashScreen/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue, Colors.teal,],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          stops: [0.0,1.0],
+          tileMode: TileMode.clamp,
+          )
+        ),
+      ),
+    );
+  }
+}
